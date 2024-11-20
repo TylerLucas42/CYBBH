0f318e1c5397da2a24a090ffa843e87b4d38cb43
TylerLucas42 <54920752+TylerLucas42@users.noreply.github.com>
2024-11-20 16:14:17 -0500

    Update datascraper.py

--- PYTHON SCRIPTS/datascraper.py
+++ PYTHON SCRIPTS/datascraper.py
@@ -5,5 +5,8 @@ import lxml.html
 import requests
 page = requests.get('http://quotes.toscrape.com')
 tree = lxml.html.fromstring(page.content)
-authors = tree.xpath('//small[@class="author"]/text()')
+authors = tree.xpath('//small[@class="author"]/text()') #THIS IS THE IMPORTANT LINE
+        #small indicates the <tag> you want to target
+        #class="author" indicates that you want to add a condition of the tag having a certain class; if you just want a tag, delete the [] brackets and everything in them
+        #/text() indicates that what you want is the text that comes after each of the matching tags
 print ('Authors: ',authors)
